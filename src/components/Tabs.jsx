74b557af7c05dce8479bbcf9830257a7ecb26d22
Nahid Khalili <Nahid_khalili@ymail.com>
2024-08-24 00:23:40 +0330

    a state added for active tab

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -1,18 +1,33 @@
 import { tabsData } from "./tabsData";
+import { useState } from "react";
 import "./Tabs.css";
 
 const Tabs = () => {
+  const [activeTab, setActiveTab] = useState(0);
+
+  const clickHandler = (id) => {
+    setActiveTab(id);
+  };
   return (
     <>
       <div className="tabs-container">
         <ul>
           {tabsData.map((item, index) => (
-            <li className={index === 0 ? "active" : ""} key={index}>{item.title}</li>
+            <li
+              className={index === activeTab ? "active" : ""}
+              key={index}
+              onClick={() => clickHandler(index)}
+            >
+              {item.title}
+            </li>
           ))}
         </ul>
         <div className="content">
           {tabsData.map((item, index) => (
-            <div key={index} className={`desc ${index === 0? "active" : ""}`}>
+            <div
+              key={index}
+              className={`desc ${index === activeTab ? "active" : ""}`}
+            >
               {item.description}
             </div>
           ))}
